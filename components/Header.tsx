c9bf54a4951cd4c4593cf1d9b8a841919fa16420
vaibhav31s <gawadvaibhavv@gmail.com>
2023-01-02 18:35:20 +0530

    Header Part over

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,28 @@
+import {MagnifyingGlassIcon ,BellIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 0) {
+            setIsScrolled(true)
+          } else {
+            setIsScrolled(false)
+          }
+        }
+    
+        window.addEventListener('scroll', handleScroll)
+    
+        return () => {
+          window.removeEventListener('scroll', handleScroll)
+        }
+      }, [])
+    
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10 ">
         <img
           src="https://rb.gy/ulxxee"
@@ -16,9 +38,22 @@ const Header = () => {
             <li>My List</li>
         </ul>
         </div>
-        <div> </div>
+        <div className='flex items-center space-x-4 font-light'>
+            <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline " />  
+            <p className='hidden lg:inline'>Kids</p>  
+            <BellIcon className="h-6 w-6 " />
+            <Link href="/account">
+                <img
+                src="https://rb.gy/g1pwyx"
+                alt=""
+                className="cursor-pointer rounded"
+                />
+            </Link>
+        </div>
     </header>
   )
 }
+
+
 
 export default Header
