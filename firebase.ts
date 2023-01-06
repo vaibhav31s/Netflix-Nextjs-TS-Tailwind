b183316aaa12c7e6f5afd0cb41003601cfa7982e
vaibhav31s <gawadvaibhavv@gmail.com>
2023-01-07 01:41:30 +0530

    Google Auth added less go

--- firebase.ts
+++ firebase.ts
@@ -16,13 +16,13 @@ const firebaseConfig = {
   appId: "1:527754451525:web:2bd3eb3ff8211331f8612c"
 };
 
-
-
 // Initialize Firebase
 
 
 const app =!getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const auth = getAuth();
+
+
 export default app;
 export {db, auth};
